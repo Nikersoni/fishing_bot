89d993cce270d8c40c133bb450bfe3f3cae2429c
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-09 18:10:06 +0300

    Update shop.py

--- handlers/shop.py
+++ handlers/shop.py
@@ -1,20 +1,16 @@
 from telebot import types
-from bot import bot, db, save_db
 
-SHOP_ITEMS = {
-    "удочка": 500,
-    "приманка": 50
-}
+SHOP_ITEMS = {"удочка":500, "приманка":50}
 
-def register(bot):
-
+def register(bot, db, save_db):
     @bot.message_handler(commands=["shop"])
     @bot.message_handler(func=lambda m: m.text.lower() == "магазин")
     def shop_handler(message):
         user_id = str(message.from_user.id)
-        if user_id not in db["users"]:
-            db["users"][user_id] = {"money":0, "fish":{}, "dishes":{}, "items":{}, "last_bonus":0}
-        
+        user = db["users"].get(user_id)
+        if not user:
+            user = db["users"][user_id] = {"money":0, "fish":{}, "dishes":{}, "items":{"удочка":1,"приманка":5}, "last_bonus":0}
+
         markup = types.InlineKeyboardMarkup()
         for item, price in SHOP_ITEMS.items():
             markup.add(types.InlineKeyboardButton(f"{item} - {price}💰", callback_data=f"buy_{item}"))
