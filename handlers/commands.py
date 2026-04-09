5de962fea1086421266457ff911293066ed31a2e
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-09 18:21:02 +0300

    Update commands.py

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,7 +1,15 @@
-from telebot import types
+def register(bot, db, save_db):
+    @bot.message_handler(func=lambda m: m.text.lower() in ["команды","помощь"])
+    def show_help(message, db=db, save_db=save_db, markup=None):
+        text = """Список всех команд:
 
-def register(bot, db, save_db, help_message):
-
-    @bot.message_handler(commands=["commands", "help"])
-    def commands_handler(message):
-        bot.send_message(message.chat.id, help_message)
+👤 Профиль  
+💎 Донат  
+🏆 Топы  
+🎁 Ежедневный бонус  
+🎣 Рыбачить  
+💰 Продать  
+🛒 Магазин  
+🍳 Готовить  
+📝 Команды"""
+        bot.send_message(message.chat.id, text, reply_markup=markup)
